ba72456751c9ec3ae9a3da40992bbcb1ce0df9c4
WizardKingAsta <maanavpilania63@gmail.com>
2025-08-25 19:54:59 -0500

    Intake user submission, send to Api. Retrieve from api, display

---
+++ web/pages/api/queue.js
@@ -0,0 +1,25 @@
+//Async function for await
+export default async function getUrls(request, res){
+    //Ensure the correct method for request
+    if (request.method == 'GET'){
+        //Try to fetch from api
+    try{
+    const API_BASE = "http://127.0.0.1:8000";
+    const TARGET = API_BASE + "/queue";
+
+    //Turn data into jason for return
+    const response = await fetch(TARGET);
+    const data = await response.json();
+
+    //Return json data
+    res.status(200).json(data);
+    // Catch error
+    }catch(error){
+        console.error(error);
+        res.status(500).json({error: "Could not fetch url"});
+    }
+    }else{
+        res.setHeader('Allow', ['GET']);
+        res.status(405).json({ error: "Invalid operation (/api/queue)"});
+    }
+}
